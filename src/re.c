89bd8c6b5f526a0aee477c62a99e191f907577cd
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2023-08-06 20:55:06 +0330

    Simplify

--- src/re.c
+++ src/re.c
@@ -5,42 +5,32 @@
 
 RE *RE_new() {
     RE *re = (RE *)malloc(sizeof(RE));
-    if (!re)
+    if (!re) {
         return NULL;
+    }
     re->special = 0;
     re->star = 0;
     return re;
 }
 
 static int RE_compile_helper(char *regex, vector *vec) {
-    if (!*regex)
+    if (!*regex) {
         return 0;
+    }
     RE *re;
-    if (!(re = RE_new()))
+    if (!(re = RE_new())) {
         return -1;
+    }
     if (*regex == '\\') {
-        switch (regex[1]) {
-        case '\0':
+        if (!regex[1]) {
             return -1;
-        case 'n':
-            re->chr = '\n';
-            break;
-        case 't':
-            re->chr = '\t';
-            break;
-        default:
-            re->chr = regex[1];
         }
+        re->chr = regex[1] == 't' ? '\t' : regex[1];
         regex += 2;
         goto next;
     }
     re->chr = *regex;
-    switch (*regex) {
-    case '^':
-    case '$':
-    case '.':
-        re->special = 1;
-    }
+    re->special = (*regex == '^' || *regex == '$' || *regex == '.');
     regex++;
     if (*regex == '*') {
         re->star = 1;
@@ -53,8 +43,9 @@ next:
 
 RE **RE_compile(char *regex) {
     vector *vec;
-    if (!(vec = vector_new()))
+    if (!(vec = vector_new())) {
         return NULL;
+    }
     if (RE_compile_helper(regex, vec) < 0) {
         vector_free_all(vec);
         return NULL;
