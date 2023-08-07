6786cc21aaac43cd5f9fe41c1db0a0dc45251cb3
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2023-08-07 09:13:48 +0330

    Implement + and ?

--- src/matcher.c
+++ src/matcher.c
@@ -7,6 +7,7 @@ static int match_helper(RE **regex, char *text);
 static int match_one(RE *re, char text);
 static int match_here(RE **regex, char *text);
 static int match_star(RE **regex, char *text, RE *re);
+static int match_maybe(RE **regex, char *text, RE *re);
 
 int match(vector *regex, char *text) {
     return match_helper((RE **)regex->elems, text);
@@ -38,8 +39,11 @@ static int match_here(RE **regex, char *text) {
     if (regex[0]->control && regex[0]->chr == '$') {
         return !text[0];
     }
-    if (regex[0]->closure == RE_STAR) {
+    if (regex[0]->closure == RE_STAR || regex[0]->closure == RE_PLUS) {
         return match_star(regex + 1, text, regex[0]);
+    }
+    if (regex[0]->closure == RE_MAYBE) {
+        return match_maybe(regex + 1, text, regex[0]);
     }
     if (match_one(regex[0], text[0])) {
         return match_here(++regex, ++text);
@@ -48,10 +52,20 @@ static int match_here(RE **regex, char *text) {
 }
 
 static int match_star(RE **regex, char *text, RE *re) {
+    if (re->closure == RE_PLUS && !match_one(re, *(text++))) {
+        return 0;
+    }
     do {
         if (match_here(regex, text)) {
             return 1;
         }
     } while (match_one(re, *(text++)));
     return 0;
+}
+
+static int match_maybe(RE **regex, char *text, RE *re) {
+    if (match_here(regex, text)) {
+        return 1;
+    }
+    return match_one(re, text[0]) && match_here(regex, text + 1);
 }
