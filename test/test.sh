a0e6580be82b40c578f4fa300ea597c503dcd055
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2023-08-07 11:56:33 +0330

    Show test status with colors

--- test/test.sh
+++ test/test.sh
@@ -13,6 +13,8 @@ PATTERNS=(
     '^b+a+b+$'
     'b$'
     'a$'
+    '^[ab]+$'
+    '^(aa)+$'
 )
 
 if [[ -z "${REEZ}" ]]; then
@@ -20,23 +22,38 @@ if [[ -z "${REEZ}" ]]; then
     exit 1
 fi
 
+show-and-run() {
+    echo '== '"$@"
+    "$@"
+}
+
+echook() {
+    echo -e '\e[1;32m[OK] '"$@"'\e[0m'
+}
+
+echobad() {
+    echo -e '\e[1;31m[BAD] '"$@"'\e[0m'
+}
+
 failed=0
 for pattern in "${PATTERNS[@]}"; do
-    if diff <($REEZ $pattern $SAMPLE 2>/dev/null) <(grep -E $pattern $SAMPLE 2>/dev/null) &>/dev/null; then
-        echo "[OK] $pattern"
+    if diff \
+        <($REEZ "$pattern" $SAMPLE 2>/dev/null) \
+        <(grep -E "$pattern" $SAMPLE 2>/dev/null) \
+        >/dev/null 2>&1
+    then
+        echook "$pattern"
     else
-        echo "[BAD] $pattern"
-        set -x
-        $REEZ $pattern $SAMPLE
-        grep -E $pattern $SAMPLE
-        set +x
+        echobad "$pattern"
+        show-and-run $REEZ "$pattern" $SAMPLE
+        show-and-run grep -E "$pattern" $SAMPLE
         failed=1
     fi
 done
 
 if (( failed == 1 )); then
-    echo '[BAD] failed at least one test' >&2
+    echobad 'failed at least one test'
     exit 1
 fi
 
-echo '[OK] passed all tests'
+echook 'passed all tests'
