3b14c3f41a6311d6dfbeb54559ef7cc080ed2225
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2023-08-07 09:21:11 +0330

    Add docs for matcher

--- src/matcher.h
+++ src/matcher.h
@@ -3,6 +3,10 @@
 
 #include "vector.h"
 
+/*
+ * Given a vector of REs _regex_, searches them inside null-terminated
+ * _text_. Doesn't change _regex_. Returns 1 if found, 0 otherwise.
+ */
 int match(vector *regex, char *text);
 
 #endif
